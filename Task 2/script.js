eea23205be7582060206ba873383569779c26f43
Rimante Merkeviciute <rimante.merkeviciute@codeacademylt.onmicrosoft.com>
2022-01-13 18:14:09 +0200

    keitimas

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,3 +3,8 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+var count=0
+
+document.getElementById("btn__state").onclick=function(){
+    count=count+1;
+    this.innerHTML=count}
