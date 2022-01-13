4ee85c08935e7089c0c630957ce6bd81df22131a
Rimante Merkeviciute <rimante.merkeviciute@codeacademylt.onmicrosoft.com>
2022-01-13 19:22:47 +0200

    v2

--- Task 6/script.js
+++ Task 6/script.js
@@ -19,3 +19,15 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+const getUserAverageAge = (masyvas) => {
+  const {length} = masyvas;
+  return masyvas.reduce ((acc,val)=> {
+    return acc +(val.age/length);
+  }, 0);
+  };
+
+  console.log(getUserAverageAge(users));
+
+
+  const getUsersName = 
