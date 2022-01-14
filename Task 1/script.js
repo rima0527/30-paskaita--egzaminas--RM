e62738e13c8757c7d981a1ec9bd72b17c79afa05
Rimante Merkeviciute <rimante.merkeviciute@codeacademylt.onmicrosoft.com>
2022-01-14 13:15:12 +0200

    Atnaujinimas 6

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,19 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const output = document.getElementById("output");
+const form = document.querySelector("form").addEventListener("submit", fn);
+const input = document.getElementById("search");
+
+function fn(e) {
+  e.preventDefault();
+  const convertToLbs = input.value * 2.2046;
+  const convertToG = input.value / 0.0010000;
+  const convertToOz = input.value * 35.274;
+  output.textContent = `Lb: ${convertToLbs}  Grams: ${convertToG}     
+    Oz: ${convertToOz}       `;
+
+  
+}
+
--- Task 10/script.js
+++ Task 10/script.js
@@ -1,6 +1,17 @@
 /* ------------------------------ TASK 9 ---------------------------------------------------
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funcijos
 -------------------------------------------------------------------------------------------- */
+import composition() from './math/composition.js';
+import division()  './math/division.js';
+import substraction() from './math/substraction.js';
+import multiplication() from './math/multiplication.js';
+import {one, two, three, four} from './numbers/numbers.js';
+
+/*import one from './numbers/numbers.js';
+import two from './numbers/numbers.js';
+import three from './numbers/numbers.js';
+import four from './numbers/numbers.js';*/
+
 
 let a = composition(one, four);
 let b = division(four, two);
